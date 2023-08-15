7558b76c0edc78119073942b21f4ae2767a9fc28
opluss <opluss@qq.com>
2023-08-15 15:00:35 +0800

    misc: bump to 0.0.7

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ class BuildQtFileCommand(build_py):
 
 setup(
     name='onnxeditor',
-    version='0.0.6',
+    version='0.0.7',
     packages=find_packages(),
     install_requires=[
         'numpy',
